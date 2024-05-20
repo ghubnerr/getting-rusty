a30d869c4b3fff30a11f0236bd22b5396448889f
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-05-19 22:17:55 -0400

    initial setup for dsa

---
+++ data-structures-and-algorithms/src/lib.rs
@@ -0,0 +1,19 @@
+mod bst;
+mod complexity;
+mod graphs;
+mod hashmaps;
+mod linkedlists;
+mod sorting;
+mod stacks;
+
+pub use bst::*;
+pub use complexity::*;
+pub use graphs::*;
+pub use hashmaps::*;
+pub use linkedlists::*;
+pub use sorting::*;
+pub use stacks::*;
+
+pub fn main() {
+    println!("Hello, world!");
+}
