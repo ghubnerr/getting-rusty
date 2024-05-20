b2c1aa4e38dcd985ac7b72789a7e107ff3a1a585
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-05-19 20:41:26 -0400

    finish first config

--- road_race/src/main.rs
+++ road_race/src/main.rs
@@ -1,27 +1,30 @@
 #![allow(unused_variables, dead_code, unused_imports)]
-
+use bevy::prelude::*;
+use rand::prelude::*;
 use rusty_engine::prelude::*;
 
 #[derive(Resource)]
 struct GameState {
-    // high_score: u32,
-    current_score: u32,
+    high_score: u32,
+    score: u32,
     ferris_index: i32,
-    // spawn_timer: Timer,
+    spawn_timer: Timer,
 }
 
 impl Default for GameState {
     fn default() -> Self {
         Self {
-            // high_score: 0,
+            high_score: 0,
             ferris_index: 0,
-            current_score: 0,
-            // spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+            score: 0,
+            spawn_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
         }
     }
 }
 fn main() {
     let mut game = Game::new();
+
+    game.audio_manager.play_music(MusicPreset::Classy8Bit, 0.2);
 
     let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
 
@@ -30,15 +33,35 @@ fn main() {
     player.scale = 1.0;
     player.collision = true;
 
-    let ferris = game.add_sprite("ferris1", "ferris.png");
-    ferris.translation = Vec2::new(100.0, 100.0);
-    ferris.collision = true;
-    ferris.scale = 0.1;
+    let score = game.add_text("score", "Score: 0");
+    score.translation = Vec2::new(520.0, 320.0);
+
+    let high_score = game.add_text("high_score", "High Score: 0");
+    high_score.translation = Vec2::new(-520.0, 320.0);
+
+    // let ferris = game.add_sprite("ferris1", "ferris.png");
+    // ferris.translation = Vec2::new(100.0, 100.0);
+    // ferris.collision = true;
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // quit if Q is pressed
+    if engine.keyboard_state.just_pressed(KeyCode::Q) {
+        engine.should_exit = true;
+    }
+
+    // keep text near the edges of the screen
+    let offset = ((engine.time_since_startup_f64 * 3.0).cos() * 5.0) as f32;
+    let score = engine.texts.get_mut("score").unwrap();
+    score.translation.x = engine.window_dimensions.x / 2.0 - 80.0;
+    score.translation.y = engine.window_dimensions.y / 2.0 - 30.0;
+
+    let high_score = engine.texts.get_mut("high_score").unwrap();
+    high_score.translation.x = engine.window_dimensions.x / 2.0 + 110.0;
+    high_score.translation.y = engine.window_dimensions.y / 2.0 - 30.0 + offset;
+
     // Collisions
     // engine.show_colliders = true; // requires valid .collider file -> run collider <path> after installing --example collider
     for event in engine.collision_events.drain(..) {
@@ -48,8 +71,15 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                     engine.sprites.remove(&label);
                 }
             }
-            game_state.current_score += 1;
-            println!("Current score: {}", game_state.current_score);
+            game_state.score += 1;
+            let score = engine.texts.get_mut("score").unwrap();
+            score.value = format!("Score: {}", game_state.score);
+            println!("Current score: {}", game_state.score);
+            if game_state.score > game_state.high_score {
+                game_state.high_score = game_state.score;
+                let high_score = engine.texts.get_mut("high_score").unwrap();
+                high_score.value = format!("High Score: {}", game_state.high_score);
+            }
         }
     }
 
@@ -83,5 +113,33 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
         player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
     }
 
-    // mouse events
+    // Mouse Events
+    if engine.mouse_state.just_pressed(MouseButton::Left) {
+        if let Some(mouse_location) = engine.mouse_state.location() {
+            let label = format!("ferris{}", game_state.ferris_index);
+            game_state.ferris_index += 1;
+            let ferris = engine.add_sprite(label.clone(), "ferris.png");
+            ferris.translation = mouse_location;
+            ferris.scale = 0.1;
+            ferris.collision = true;
+        }
+    }
+
+    if game_state.spawn_timer.tick(engine.delta).just_finished() {
+        let label = format!("ferris{}", game_state.ferris_index);
+        game_state.ferris_index += 1;
+        let ferris = engine.add_sprite(label.clone(), "ferris.png");
+        ferris.translation.x = thread_rng().gen_range(-550.0..550.0);
+        ferris.translation.y = thread_rng().gen_range(-325.0..325.0);
+        ferris.scale = 0.1;
+        ferris.collision = true;
+    }
+
+    // Reset Score
+    if engine.keyboard_state.just_pressed(KeyCode::F) {
+        game_state.score = 0;
+        let score = engine.texts.get_mut("score").unwrap();
+        score.value = format!("Score: {}", game_state.score);
+    }
+    engine.audio_manager.play_sfx(SfxPreset::Minimize1, 0.1);
 }
